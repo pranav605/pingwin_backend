5de0bf74b0afb3375f98f29a8e680cd760d21ea0
pranav605 <pranav605@gmail.com>
2026-04-27 20:12:35 -0400

    Trusting proxies for rate limiter

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import rateLimit from 'express-rate-limit'
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.set('trust proxy', 1)
+
 // ── Middleware ───────────────────────────────────
 app.use(cors())
 app.use(express.json())
